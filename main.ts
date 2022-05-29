15f06b603cd91a82fd01f9843ac516969a49a10a
tranka2010 <106484816+tranka2010@users.noreply.github.com>
2022-05-29 18:48:33 -0400

    Added robustness thru loud sound detection, fixed BT events

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
-input.onGesture(Gesture.EightG, function () {
-    punchDetected = 1
-})
 bluetooth.onBluetoothConnected(function () {
     bluetooth.startUartService()
     bluetooth.setTransmitPower(7)
     images.createImage(`
-        # . # # .
-        . # # . #
         . . # # .
-        . # # . #
-        # . # # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . . # # .
         `).showImage(0)
 })
 bluetooth.onBluetoothDisconnected(function () {
@@ -18,27 +15,40 @@ bluetooth.onBluetoothDisconnected(function () {
 input.onButtonPressed(Button.A, function () {
     punchCount = 0
 })
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Fullstop), function () {
-    images.createImage(`
-        # # # . .
-        . # . . .
-        . # # . #
-        . . . # .
-        . . # . #
-        `).showImage(0)
-    punchCount = 0
-    punchDetected = 0
-    bluetooth.uartWriteLine("" + control.eventTimestamp() + "," + punchCount + ",")
+bluetooth.onUartDataReceived(bluetooth.uartReadUntil(serial.delimiters(Delimiters.CarriageReturn)), function () {
+    if ("R" == bluetooth.uartReadUntil(serial.delimiters(Delimiters.CarriageReturn))) {
+        images.createImage(`
+            # # . . .
+            # . # . .
+            # # . . .
+            # . # . .
+            # . # . .
+            `).showImage(0)
+        bluetooth.uartWriteLine("" + control.eventTimestamp() + "," + punchCount + ",")
+    } else if ("C" == bluetooth.uartReadUntil(serial.delimiters(Delimiters.CarriageReturn))) {
+        images.createImage(`
+            . # # . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . # # . .
+            `).showImage(0)
+        punchCount = 0
+    } else {
+        images.createImage(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `).showImage(0)
+    }
 })
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
-    images.createImage(`
-        # # # . .
-        . # . . .
-        . # # . #
-        . . . # .
-        . . # . #
-        `).showImage(0)
-    bluetooth.uartWriteLine("" + control.eventTimestamp() + "," + punchCount + ",")
+input.onGesture(Gesture.SixG, function () {
+    punchDetected = 1
+})
+input.onSound(DetectedSound.Loud, function () {
+    loudSound += 1
 })
 input.onButtonPressed(Button.B, function () {
     images.createImage(`
@@ -50,17 +60,21 @@ input.onButtonPressed(Button.B, function () {
         `).showImage(0)
     basic.showString("" + (punchCount))
 })
+let loudSound = 0
 let punchCount = 0
 let punchDetected = 0
 punchDetected = 0
+input.setAccelerometerRange(AcceleratorRange.EightG)
 basic.forever(function () {
-    if (punchDetected) {
+    if (punchDetected > 0 && loudSound > 0) {
         punchCount += 1
         punchDetected = 0
-        music.playTone(262, music.beat(BeatFraction.Quarter))
+        loudSound = 0
+        music.playTone(277, music.beat(BeatFraction.Quarter))
         images.iconImage(IconNames.No).showImage(0)
-        basic.pause(100)
+        basic.pause(50)
     } else {
+        loudSound = 0
         images.createImage(`
             . . . . .
             . . . . .
