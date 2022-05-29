22f36234950c4075b211fc328fa34bc11f308bab
tranka2010 <106484816+tranka2010@users.noreply.github.com>
2022-05-29 16:45:23 -0400

    More updates

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
+input.onGesture(Gesture.EightG, function () {
+    punchDetected = 1
+})
 bluetooth.onBluetoothConnected(function () {
-    bluetooth.setTransmitPower(7)
     bluetooth.startUartService()
+    bluetooth.setTransmitPower(7)
     images.createImage(`
-        # # # . .
-        # . . # .
-        # # # . .
-        # . . # .
-        # # # . .
+        # . # # .
+        . # # . #
+        . . # # .
+        . # # . #
+        # . # # .
         `).showImage(0)
 })
 bluetooth.onBluetoothDisconnected(function () {
@@ -15,10 +18,26 @@ bluetooth.onBluetoothDisconnected(function () {
 input.onButtonPressed(Button.A, function () {
     punchCount = 0
 })
-input.onGesture(Gesture.SixG, function () {
-    punchDetected = 1
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Fullstop), function () {
+    images.createImage(`
+        # # # . .
+        . # . . .
+        . # # . #
+        . . . # .
+        . . # . #
+        `).showImage(0)
+    punchCount = 0
+    punchDetected = 0
+    bluetooth.uartWriteLine("" + control.eventTimestamp() + "," + punchCount + ",")
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
+    images.createImage(`
+        # # # . .
+        . # . . .
+        . # # . #
+        . . . # .
+        . . # . #
+        `).showImage(0)
     bluetooth.uartWriteLine("" + control.eventTimestamp() + "," + punchCount + ",")
 })
 input.onButtonPressed(Button.B, function () {
@@ -37,10 +56,10 @@ punchDetected = 0
 basic.forever(function () {
     if (punchDetected) {
         punchCount += 1
-        music.playTone(262, music.beat(BeatFraction.Half))
-        images.iconImage(IconNames.No).showImage(0)
-        basic.pause(150)
         punchDetected = 0
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        images.iconImage(IconNames.No).showImage(0)
+        basic.pause(100)
     } else {
         images.createImage(`
             . . . . .
