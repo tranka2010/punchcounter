8f690d113a4fe6962227096905a309101b243c77
tranka2010 <106484816+tranka2010@users.noreply.github.com>
2022-06-09 21:58:54 -0400

    Remove punch displays, keep speaker disabled when not in user

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onGesture(Gesture.EightG, function () {
     punchDetected = 1
 })
 bluetooth.onBluetoothConnected(function () {
+    music.setBuiltInSpeakerEnabled(true)
     music.playTone(554, music.beat(BeatFraction.Quarter))
     BTconnected = 1
     images.createImage(`
@@ -11,11 +12,14 @@ bluetooth.onBluetoothConnected(function () {
         . # # . #
         # . # # .
         `).showImage(0)
+    music.setBuiltInSpeakerEnabled(false)
 })
 bluetooth.onBluetoothDisconnected(function () {
+    music.setBuiltInSpeakerEnabled(true)
     music.playTone(139, music.beat(BeatFraction.Quarter))
     BTconnected = 0
     images.iconImage(IconNames.Skull).showImage(0)
+    music.setBuiltInSpeakerEnabled(false)
 })
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + punchCount)
@@ -28,12 +32,14 @@ input.onButtonPressed(Button.B, function () {
 let BTconnected = 0
 let punchCount = 0
 let punchDetected = 0
+music.setBuiltInSpeakerEnabled(true)
 punchDetected = 0
 punchCount = 0
 music.playTone(392, music.beat(BeatFraction.Quarter))
 bluetooth.startUartService()
 bluetooth.setTransmitPower(7)
 led.setBrightness(64)
+music.setBuiltInSpeakerEnabled(false)
 basic.forever(function () {
     if (punchDetected > 0) {
         punchCount += 1
@@ -41,19 +47,5 @@ basic.forever(function () {
         if (BTconnected == 1) {
             bluetooth.uartWriteLine("" + convertToText(control.eventTimestamp()) + "," + convertToText(punchCount) + ",")
         }
-        images.createImage(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `).showImage(0)
     }
-    images.createImage(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `).showImage(0)
 })
